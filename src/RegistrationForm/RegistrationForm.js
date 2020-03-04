9278f100c6164c7129cb92a7a7f2d2b653438f16
Blake Lundquist <lundqub@outlook.com>
2020-03-03 17:59:20 -0800

    uncontrolled validation

--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -4,36 +4,29 @@ class RegistrationForm extends Component {
 
   constructor(props) {
     super(props);
+    this.ssnRef = React.createRef();
     this.state = {
-      ssn: {
-        value: '',
-        touched: false
-      }
-
+      errorMessage: ''
     }
   }
 
-  setSsn = ssn => {
-    this.setState({ssn: {value: ssn, touched: true}});
-  };
+  handleSubmit = (event) => {
+    event.preventDefault()
+    let inputData = this.ssnRef.current.value
+    this.validateSsn(inputData)
+  }
 
-  validateSsn = () => {
-    if (this.state.ssn.touched) {
-      let ssn = this.state.ssn.value
-      ssn = ssn.replace(/[\s-]/g, ''); // Remove whitespace and dashes
-      if (ssn.length !== 9) {
-          // Check if it's 9 characters long
-          return 'SSN must be 9 digits long';
-      } else if (!/^\d+$/.test(ssn)) {
-          // Check if it's just digits
-          return 'SSN must only contain numbers';
-      }
+  validateSsn = (ssn) => {
+    if (ssn.length === 9) {
+      this.setState({ errorMessage: ''})
+    } else {
+      this.setState({ errorMessage: 'SSN must be exactly 9 characters'})
     }
   }
 
   render() {
     return (
-        <form className="registration">
+        <form className="registration" onSubmit={(event) => this.handleSubmit(event)}>
           <h2>Register</h2>
           <div className="form-group">
             <label htmlFor="ssn">SSN*</label>
@@ -42,20 +35,18 @@ class RegistrationForm extends Component {
               className="registration__control"
               name="ssn"
               id="ssn"
-              value={this.state.ssn.value}
-              onChange={(event) => this.setSsn(event.target.value)}
+              ref={this.ssnRef}
             />
           </div>
           <div className="registration__button__group">
             <button
               type="submit"
               className="registration__button"
-              disabled={!!this.validateSsn()}
             >
               Save
             </button>
           </div>
-          {this.validateSsn() ? <p>{this.validateSsn()}</p> : ''}
+          {this.state.errorMessage ? <p>{this.state.errorMessage}</p> : ''}
         </form>
     );
   }
